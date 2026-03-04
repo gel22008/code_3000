93dc559cbcbcebbc53aca61dff131c5dfd1fbc4e
unknown <gabriella.luciani@uconn.edu>
2026-03-04 13:56:49 -0500

    I added in all my code here from previous projects so everything here is in the forked version of the code_3000, not just a cloned version I had locally.

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -6,16 +6,19 @@ from sklearn.ensemble import GradientBoostingClassifier
 seed = 314
 
 def train_model(X, y, seed=seed):
+    #this is used to train the model every time 
     """
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
         learning_rate=0.1,
         n_estimators=100,
-        max_depth=8,
+        max_depth=2,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
     )
+    #where model finds patterns in X, or the training file
+    #that are then used to test the files in Y
     model.fit(X, y)
     return model
