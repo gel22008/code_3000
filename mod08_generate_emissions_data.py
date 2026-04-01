86882a73e94884cc8624a9d4eab2cd3ec92417b2
unknown <gabriella.luciani@uconn.edu>
2026-04-01 19:54:50 -0400

    Wrote down answers to the 3 questions, and also changed the [0 1] value

--- mod08_generate_emissions_data.py
+++ mod08_generate_emissions_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def generate_data():
+    #table of jobs generated with below data
+    #job number, how many CPU cores job needs
+    #and how long jor runs in hours
     jobs = pd.DataFrame({
         "job_id": range(1, 16),
         "cpu_cores": [1, 2, 4, 8, 2, 1, 16, 4, 8, 2, 4, 8, 2, 8, 16],
@@ -8,6 +11,8 @@ def generate_data():
     })
 
     carbon_intensity = {
+        #how intense the carbon is, 
+            #
         "coal_heavy_region": 0.9,   # kg CO2 per kWh
     #    "mixed_region": 0.45,
         "renewable_region": 0.05
@@ -16,9 +21,12 @@ def generate_data():
     power_per_core_kw = 0.05  # 50W per CPU core
 
     max_cores_per_region = {
+        #computing capacity per cores for this region
         "coal_heavy_region": 32,
     #   "mixed_region": 24,
         "renewable_region": 16
     }
 
+    #returned so could be used in sustainability 
+    # calculations file
     return jobs, carbon_intensity, power_per_core_kw, max_cores_per_region
